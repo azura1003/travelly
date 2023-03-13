e58395fe3d9793774593ca16cdad811048a02601
azura1003 <noorkhan.95150@gmail.com>
2023-03-13 22:52:55 +0100

    ajout login pop up

---
+++ file_js/login-popup.js
@@ -0,0 +1,6 @@
+document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector(".pop-up").classList.add("active");
+});
+document.querySelector(".pop-up .close-btn").addEventListener("click", function(){
+    document.querySelector(".pop-up").classList.remove("active");
+});
