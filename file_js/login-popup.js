443e9d9130d7ff29ae9a816c674ff395422c019c
azura1003 <noorkhan.95150@gmail.com>
2023-03-14 01:23:36 +0100

    ajout js ouverture fermeture pop

--- file_js/login-popup.js
+++ file_js/login-popup.js
@@ -1,8 +1,44 @@
-document.querySelector("#show-login").addEventListener("click", function(){
-    document.querySelector(".pop-up").classList.add("active");
-});
-document.querySelector(".pop-up .close-btn").addEventListener("click", function(){
-    document.querySelector(".pop-up").classList.remove("active");
+var signinPopup = document.querySelector(".pop-up2");
+var showSigninButton = document.querySelector("#show-signin");
+var signinCloseButton = document.querySelector(".close-btn2");
+var page = document.querySelector("body");
+
+showSigninButton.addEventListener("click", function(){
+  if (signinPopup.classList.contains("active")) {
+    signinPopup.classList.remove("active");
+  } else {
+    signinPopup.classList.add("active");
+  }
 });
 
+signinCloseButton.addEventListener("click", function(){
+  signinPopup.classList.remove("active");
+});
 
+page.addEventListener("click", function(event){
+  if(event.target !== signinPopup && !signinPopup.contains(event.target) && event.target !== showSigninButton) {
+    signinPopup.classList.remove("active");
+  }
+});
+
+var loginPopup = document.querySelector(".pop-up");
+var showLoginButton = document.querySelector("#show-login");
+var loginCloseButton = document.querySelector(".close-btn");
+
+showLoginButton.addEventListener("click", function(){
+  if (loginPopup.classList.contains("active")) {
+    loginPopup.classList.remove("active");
+  } else {
+    loginPopup.classList.add("active");
+  }
+});
+
+loginCloseButton.addEventListener("click", function(){
+  loginPopup.classList.remove("active");
+});
+
+page.addEventListener("click", function(event){
+  if(event.target !== loginPopup && !loginPopup.contains(event.target) && event.target !== showLoginButton) {
+    loginPopup.classList.remove("active");
+  }
+});
